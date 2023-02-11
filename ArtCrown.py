15117c5eae0c95a98018fd8f108128ed98ea501f
jangddol <j991005@korea.ac.kr>
2023-02-12 00:57:02 +0900

    fix ArtFlower etc.

--- ArtCrown.py
+++ ArtCrown.py
@@ -1,12 +1,12 @@
-import Artifact
+from Artifact import Artifact
 
 
 class ArtCrown(Artifact):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
         self._mType = 5
         self._mProbabilityWeight = [ 5,  5, 11,  0,  0, 11,  0,  2, 11,  0,  0,  0,  0,  0,  0,  0,  0,  0,  5 ]
         self._mCummulatedWeight  = [ 5, 10, 21, 21, 21, 32, 32, 34, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 50 ]
     
-    def GetPossibleMainOption():
+    def GetPossibleMainOption(self):
         return [0, 1, 2, 5, 7, 8, 18]
--- ArtFlower.py
+++ ArtFlower.py
@@ -1,13 +1,13 @@
-import Artifact
+from Artifact import Artifact
 
 
 class ArtFlower(Artifact):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
         self._mType = 1
         self._mProbabilityWeight = [ 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
         self._mCummulatedWeight = [ 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
         self.SetMainType(6)
     
-    def GetPossibleMainOption():
+    def GetPossibleMainOption(self):
         return [6]
